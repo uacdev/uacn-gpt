0f68b6c243eae5b4d54dfb263e039583a8e8b184
uacdev <shop_d2c@uacfoodsng.com>
2026-03-29 13:25:09 +0100

    5th commit

--- Landing page/src/main.tsx
+++ Landing page/src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Allow natural touch scrolling
+document.addEventListener("touchmove", (e) => {
+  // Allow scrolling - don't prevent default
+  return true;
+}, { passive: true });
+
 createRoot(document.getElementById("root")!).render(<App />);
